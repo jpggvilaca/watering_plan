b1d4b162cbf99d8b9b79f425fef428e2f0872a62
jpggvilaca <jpggvilaca@gmail.com>
2015-04-20 00:10:56 +0100

    Refactored sendinfo tag

--- build/js/sendinfo.js
+++ build/js/sendinfo.js
@@ -1,21 +1,18 @@
-riot.tag('sendinfo', '<form> <span class="form-title">Please fill up the form</span> <span>Temperature</span> <input type="text"></input> <span>Field size</span> <input type="text"></input> <input type="submit" value="Submit data" id="submitMe"> </form>', function(opts) {
+riot.tag('sendinfo', '<form onsubmit="{ submitted }"> <span class="form-title">Please fill up the form</span> <span>Temperature</span> <input type="text"></input> <span>Field size</span> <input type="text"></input> <input type="submit" value="Submit data" id="submitMe"></input> </form> <p> Your coordinates are { longitude }, { latitude }. </p>', function(opts) {
+	this.submitted = function(e) {
+		e.preventDefault();
 
-	this.items = [
-	    { title: 'Temperatura'},
-	    { title: 'Humidade' },
-	    { title: 'Precipitação' }
-	];
+		var data = $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk", function(json) {
+				console.log(json.coord);
+				this.longitude = json.coord.lon; 
+				this.latitude = json.coord.lat;
 
-	this.on('mount', function () {
-		document.querySelector('.form-title').addEventListener("click", function(){
-			console.log("it worked!");
-		});
+			});
 
-		document.querySelector('#submitMe').addEventListener("click", function(e){
-			e.preventDefault();
-			console.log("submitted!!");
-		});
-	});
+		this.update();
+
+		console.log("form submitted successfully");
+	}.bind(this);
 
 	
 });
--- client/tags/sendinfo.tag
+++ client/tags/sendinfo.tag
@@ -1,32 +1,41 @@
 <sendinfo>
 
-	<form>
+	<form onsubmit={ submitted }>
         <span class="form-title">Please fill up the form</span>
         <span>Temperature</span>
         <input type="text"></input>
         <span>Field size</span>
         <input type="text"></input>
-        <input type="submit" value="Submit data" id="submitMe">
+        <input type="submit" value="Submit data" id="submitMe"></input>
 	</form>
+
+	<p>
+		Your coordinates are { longitude }, { latitude }.
+	</p>
 
 	<script>
 
-	this.items = [
-	    { title: 'Temperatura'},
-	    { title: 'Humidade' },
-	    { title: 'Precipitação' }
-	];
 
-	this.on('mount', function () {
-		document.querySelector('.form-title').addEventListener("click", function(){
-			console.log("it worked!");
-		});
+	// Send API call after receiving user data
+	submitted(e) {
+		e.preventDefault();
 
-		document.querySelector('#submitMe').addEventListener("click", function(e){
-			e.preventDefault();
-			console.log("submitted!!");
-		});
-	});
+		var data = $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,uk", function(json) {
+				console.log(json.coord);
+				this.longitude = json.coord.lon; 
+				this.latitude = json.coord.lat;
+
+			});
+
+		this.update();
+
+		console.log("form submitted successfully");
+	};
+
+	// Get the data and format it
+
+
+	// Display the result to the user
 
 	</script>
 
