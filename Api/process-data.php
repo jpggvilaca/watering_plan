cd494c10ea67ba08d1cecbad9a74b5ae5e7f0323
jpggvilaca <jpggvilaca@gmail.com>
2015-04-28 23:10:25 +0100

    wip on write to file

--- Api/process-data.php
+++ Api/process-data.php
@@ -1,1 +1,12 @@
 // Writes data to .txt in order to send it to octave
+
+header('HTTP/1.1 200' );
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, WCTrustedToken, userId, WCToken, PersonalizationID, AUTHUSER, Primarynum');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
+
+$myfile = fopen("cenas.txt", "w") or die("Unable to open file!");
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
